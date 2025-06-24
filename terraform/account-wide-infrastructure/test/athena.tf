43b2e4e5aaa8cfe5c164a786a17c1607710bc4a2
Matt Dean <matt.dean3@nhs.net>
2025-06-24 15:36:04 +0100

    [NRL-1386] Remove env-specific reporting infra from account-wide

--- terraform/account-wide-infrastructure/test/athena.tf
+++ terraform/account-wide-infrastructure/test/athena.tf
@@ -1,31 +1,3 @@
-/*module "qa-athena" {
-  source             = "../modules/athena"
-  name_prefix        = "nhsd-nrlf--qa"
-  target_bucket_name = module.qa-glue.target_bucket_name
-  glue_database      = module.qa-glue.glue_database
-}
-
-module "int-athena" {
-  source             = "../modules/athena"
-  name_prefix        = "nhsd-nrlf--int"
-  target_bucket_name = module.int-glue.target_bucket_name
-  glue_database      = module.int-glue.glue_database
-}
-
-module "int-sandbox-athena" {
-  source             = "../modules/athena"
-  name_prefix        = "nhsd-nrlf--int-sandbox"
-  target_bucket_name = module.int-sandbox-glue.target_bucket_name
-  glue_database      = module.int-sandbox-glue.glue_database
-}
-
-module "ref-athena" {
-  source             = "../modules/athena"
-  name_prefix        = "nhsd-nrlf--ref"
-  target_bucket_name = module.ref-glue.target_bucket_name
-  glue_database      = module.ref-glue.glue_database
-}*/
-
 module "test-athena" {
   count              = var.enable_reporting ? 1 : 0
   source             = "../modules/athena"
--- terraform/account-wide-infrastructure/test/glue.tf
+++ terraform/account-wide-infrastructure/test/glue.tf
@@ -1,27 +1,3 @@
-/*module "qa-glue" {
-  source         = "../modules/glue"
-  name_prefix    = "nhsd-nrlf--qa"
-  python_version = 3
-}
-
-module "int-glue" {
-  source         = "../modules/glue"
-  name_prefix    = "nhsd-nrlf--int"
-  python_version = 3
-}
-
-module "int-sandbox-glue" {
-  source         = "../modules/glue"
-  name_prefix    = "nhsd-nrlf--int-sandbox"
-  python_version = 3
-}
-
-module "ref-glue" {
-  source         = "../modules/glue"
-  name_prefix    = "nhsd-nrlf--ref"
-  python_version = 3
-}*/
-
 module "test-glue" {
   is_enabled     = var.enable_reporting
   source         = "../modules/glue"
