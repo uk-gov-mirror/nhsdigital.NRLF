a3c9760956987a2fab84418c39b9187a5b7d713b
Axel Garcia K. <axel.krastek1@nhs.net>
2025-08-21 15:29:25 +0100

    NRL-1491 Add InContext retrieval mechanism and allow text/html with structured format

--- layer/nrlf/core/constants.py
+++ layer/nrlf/core/constants.py
@@ -687,7 +687,8 @@ CONTENT_RETRIEVAL_SYSTEM_URL = (
 CONTENT_RETRIEVAL_CODE_MAP = {
     "Direct": "Direct",
     "SSP": "Spine Secure Proxy",
-    "LDR": "Large Document Retrieval",
+    "NDR": "Large Document Retrieval",
+    "InContext": "Direct using In-Context",
 }
 CONTENT_FORMAT_CODE_URL = "https://fhir.nhs.uk/England/CodeSystem/England-NRLFormatCode"
 CONTENT_FORMAT_CODE_MAP = {
