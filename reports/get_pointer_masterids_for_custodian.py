d8b0a0ea63089c38c58c06191e6dd774a7794c06
Matt Dean <matt.dean3@nhs.net>
2025-09-22 15:31:37 +0100

    [NRL-1606] Add report to get masterids for pointers

---
+++ reports/get_pointer_masterids_for_custodian.py
@@ -0,0 +1,80 @@
+import json
+from datetime import datetime, timedelta, timezone
+from typing import Any
+
+import boto3
+import fire
+
+dynamodb = boto3.client("dynamodb")
+paginator = dynamodb.get_paginator("scan")
+
+
+def _get_masterids_for_custodians(table_name: str, custodians: str) -> Any:
+    """
+    Get masterids for pointers in the given table for a list of custodians.
+    Parameters:
+    - table_name: The name of the pointers table to use.
+    """
+
+    print(  # noqa
+        f"Getting masterids for custodians {custodians} in table {table_name}...."
+    )
+
+    expression_names_str = ",".join([f":param_{custodian}" for custodian in custodians])
+    expression_values_list = {
+        f":param_{custodian}": {"S": custodian} for custodian in custodians
+    }
+
+    params: dict[str, Any] = {
+        "TableName": table_name,
+        "PaginationConfig": {"PageSize": 50},
+        "FilterExpression": f"custodian IN ({expression_names_str})",
+        "ExpressionAttributeValues": expression_values_list,
+        "ProjectionExpression": "id, type_id, master_identifier",
+    }
+
+    pointers_info: list[dict[str, str]] = []
+    total_scanned_count = 0
+
+    start_time = datetime.now(tz=timezone.utc)
+
+    for page in paginator.paginate(**params):
+        for item in page["Items"]:
+            pointer_id = item.get("id", {}).get("S", "no-id")
+            pointer_type = item.get("type_id", {}).get("S", "no-type")
+            master_id = item.get("master_identifier", {}).get("S", "no-master-id")
+
+            pointers_info.append(
+                {
+                    "nrl-id": pointer_id,
+                    "pointer-type": pointer_type,
+                    "master_identifier": master_id,
+                }
+            )
+
+        total_scanned_count += page["ScannedCount"]
+
+        if total_scanned_count % 1000 == 0:
+            print(".", end="", flush=True)  # noqa
+
+        if total_scanned_count % 100000 == 0:
+            print(f"scanned={total_scanned_count} found={len(pointers_info)} ")  # noqa
+
+    end_time = datetime.now(tz=timezone.utc)
+
+    print(" Done")  # noqa
+
+    print(f"Writing pointers to file ./pointer-masterids.txt ...")  # noqa
+    with open(f"pointer-masterids.txt", "w") as f:
+        f.write(json.dumps(pointers_info, indent=2))
+
+    return {
+        "output-file": "pointer-masterids.txt",
+        "pointers-found": len(pointers_info),
+        "scanned-count": total_scanned_count,
+        "took-secs": timedelta.total_seconds(end_time - start_time),
+    }
+
+
+if __name__ == "__main__":
+    fire.Fire(_get_masterids_for_custodians)
