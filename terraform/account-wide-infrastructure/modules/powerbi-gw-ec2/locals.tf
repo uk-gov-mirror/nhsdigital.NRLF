1aef75e94dcfcf3378e8c34c4557cae87de63f83
Matt Dean <matt.dean3@nhs.net>
2025-10-28 16:12:29 +0000

    [NRL-1739] Add automatic scheduled updates for Window PowerBI GW instances

--- terraform/account-wide-infrastructure/modules/powerbi-gw-ec2/locals.tf
+++ terraform/account-wide-infrastructure/modules/powerbi-gw-ec2/locals.tf
@@ -1,3 +1,4 @@
 locals {
-  selected_ami_id = var.use_custom_ami ? data.aws_ami.PowerBI_Gateway[0].id : data.aws_ami.windows-2019.id
+  selected_ami_id      = var.use_custom_ami ? data.aws_ami.PowerBI_Gateway[0].id : data.aws_ami.windows-2019.id
+  windows_patching_tag = "windows_scheduled_patching"
 }
