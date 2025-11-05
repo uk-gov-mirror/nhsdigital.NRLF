77ea10809beac8b896f3320b63fc2ffb0a478f71
Axel Garcia K. <axel.krastek1@nhs.net>
2025-11-05 12:34:07 +0000

    NRL-1664 Add perftest nameservers and fix dependency order terraform

--- terraform/account-wide-infrastructure/test/domain.tf
+++ terraform/account-wide-infrastructure/test/domain.tf
@@ -45,4 +45,5 @@ module "perftest-custom-domain-name" {
   domain_zone                   = aws_route53_zone.test-perftest-ns.name
   mtls_certificate_file         = "s3://${module.perftest-truststore-bucket.bucket_name}/${module.perftest-truststore-bucket.certificates_object_key}"
   mtls_certificate_file_version = module.perftest-truststore-bucket.certificates_object_version
+  depends_on                    = [aws_route53_zone.test-perftest-ns]
 }
