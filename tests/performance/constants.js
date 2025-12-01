a2e107605b14f240b39e3d07a102fe00f71227be
Axel Garcia K. <axel.krastek1@nhs.net>
2025-12-01 16:03:49 +0000

    NRL-1666 Updated types and categories constants

--- tests/performance/constants.js
+++ tests/performance/constants.js
@@ -14,21 +14,76 @@ export const POINTER_IDS = ALL_POINTER_IDS.slice(3500);
 export const NHS_NUMBERS = REFERENCE_DATA["nhs_numbers"];
 export const POINTER_TYPES = [
   "736253002",
-  "1363501000000100",
+  "887701000000100",
+  "861421000000109",
   "1382601000000107",
+  "1363501000000100",
   "325691000000100",
   "736373009",
-  "861421000000109",
-  "887701000000100",
+  "16521000000101",
   "736366004",
   "735324008",
   "824321000000109",
   "2181441000000107",
+  "749001000000101",
+  "887181000000106",
 ];
+
+export const POINTER_TYPE_DISPLAY = {
+  736253002: "Mental health crisis plan",
+  887701000000100: "Emergency health care plan",
+  861421000000109: "End of life care coordination summary",
+  1382601000000107: "ReSPECT (Recommended Summary Plan for Emergency Care and Treatment) form",
+  1363501000000100: "Royal College of Physicians NEWS2 (National Early Warning Score 2) chart",
+  325691000000100: "Contingency plan",
+  736373009: "End of life care plan",
+  16521000000101: "Lloyd George record folder",
+  736366004: "Advance care plan",
+  735324008: "Treatment escalation plan",
+  824321000000109: "Summary record",
+  2181441000000107: "Personalised Care and Support Plan",
+  749001000000101: "Appointment",
+  887181000000106: "Clinical summary",
+};
+
 export const CATEGORIES = [
   "734163000",
   "1102421000000108",
   "823651000000106",
-  "721981007",
-  "103693007",
+  "419891008",
+  "716931000000107",
 ];
+
+export const CATEGORY_DISPLAY = {
+  734163000: "Care plan",
+  1102421000000108: "Observations",
+  823651000000106: "Clinical note",
+  419891008: "Record artifact",
+  716931000000107: "Record headings",
+};
+
+export const TYPE_CATEGORY_MAP = {
+  // Care plans
+  736253002: "734163000",
+  887701000000100: "734163000",
+  861421000000109: "734163000",
+  1382601000000107: "734163000",
+  325691000000100: "734163000",
+  736373009: "734163000",
+  16521000000101: "734163000",
+  736366004: "734163000",
+  735324008: "734163000",
+  2181441000000107: "734163000",
+
+  // Observations
+  1363501000000100: "1102421000000108",
+
+  // Clinical notes
+  824321000000109: "823651000000106",
+
+  // Bookings and Referrals
+  749001000000101: "419891008",
+
+  // Shared Care Records
+  887181000000106: "716931000000107",
+};
