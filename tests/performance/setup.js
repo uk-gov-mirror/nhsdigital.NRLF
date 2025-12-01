5005dedc9da528769f24c496b0f772d74c3b41ca
Axel Garcia K. <axel.krastek1@nhs.net>
2025-12-01 17:03:20 +0000

    NRL-1666 Fix createRecord to make sure type and categories match with correct display values

--- tests/performance/setup.js
+++ tests/performance/setup.js
@@ -1,4 +1,10 @@
-import { POINTER_TYPES, DEFAULT_TEST_RECORD, ODS_CODE } from "./constants.js";
+import {
+  DEFAULT_TEST_RECORD,
+  ODS_CODE,
+  POINTER_TYPE_DISPLAY,
+  TYPE_CATEGORY_MAP,
+  CATEGORY_DISPLAY,
+} from "./constants.js";
 import { crypto } from "k6/experimental/webcrypto";
 
 export function createRecord(nhsNumber, pointerType) {
@@ -6,7 +12,15 @@ export function createRecord(nhsNumber, pointerType) {
   record.id = `${ODS_CODE}-${crypto.randomUUID()}`;
 
   record.type.coding[0].code = pointerType;
-  record.type.coding[0].display = POINTER_TYPES[pointerType];
+  if (!POINTER_TYPE_DISPLAY[pointerType]) {
+    throw new Error(`Display not found for pointerType: ${pointerType}`);
+  }
+  record.type.coding[0].display = POINTER_TYPE_DISPLAY[pointerType];
+
+  const categoryCode = TYPE_CATEGORY_MAP[pointerType];
+  record.category[0].coding[0].code = categoryCode;
+  record.category[0].coding[0].display = CATEGORY_DISPLAY[categoryCode];
+
   record.subject.identifier.value = nhsNumber;
   record.context.sourcePatientInfo.identifier.value = nhsNumber;
 
