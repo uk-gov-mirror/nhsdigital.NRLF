be5c8900ec6544cac8534138ab7dce7f231a265d
Anjali Trace <anjali.trace2@nhs.net>
2026-01-09 16:08:48 +0000

    NRL-1875 Consumer reference data unused. Producer reference json already in git

--- tests/performance/perftest_environment.py
+++ tests/performance/perftest_environment.py
@@ -1,7 +1,5 @@
 import csv
-import json
 import os
-import pathlib
 import re
 
 import boto3
@@ -19,41 +17,6 @@ def _get_pointers_table_name():
         return perftest_table_name
 
     return f"nhsd-nrlf--{perftest_table_name}-pointers-table"
-
-
-def extract_consumer_data(output_dir="."):
-    out = output_dir + "/consumer_reference_data.json"
-    table_name = _get_pointers_table_name()
-    table = DYNAMODB.Table(table_name)
-    scan_kwargs = {}
-    done = False
-    start_key = None
-    nhs_numbers = set()
-    pointer_ids = []
-    custodians = set()
-    while not done:
-        if start_key:
-            scan_kwargs["ExclusiveStartKey"] = start_key
-        response = table.scan(**scan_kwargs)
-        for item in response.get("Items", []):
-            nhs_number = item.get("nhs_number")
-            pointer_id = item.get("id")
-            custodian = item.get("custodian")
-            if nhs_number:
-                nhs_numbers.add(nhs_number)
-            if pointer_id:
-                pointer_ids.append(pointer_id)
-            if custodian:
-                custodians.add(custodian)
-        start_key = response.get("LastEvaluatedKey", None)
-        done = start_key is None
-    data = {
-        "nhs_numbers": list(nhs_numbers),
-        "pointer_ids": pointer_ids,
-        "custodians": list(custodians),
-    }
-    pathlib.Path(out).write_text(json.dumps(data))
-    print(f"Consumer data written to {out}")  # noqa: T201
 
 
 class TestNhsNumbersIterator:
@@ -78,99 +41,6 @@ class TestNhsNumbersIterator:
             )
         nhs_no = str(self.first9) + str(checksum)
         return nhs_no
-
-
-def generate_producer_data(
-    output_dir=".",
-    proportion_existing=0.8,  # Proportion of output that should be existing NHS numbers
-    total_count=1000,  # Total number of NHS numbers to output
-    last_existing_nhs_number=None,  # Optionally specify the last NHS number in the table
-):
-    """
-    Generate a reference dataset for producer tests, containing a mix of existing and new NHS numbers.
-    - proportion_existing: fraction of output that should be existing NHS numbers (0.0-1.0)
-    - total_count: total number of NHS numbers in output
-    NHS numbers are generated in a semi-deterministic way, similar to the NFT seeding script.
-    """
-    out = output_dir + "/producer_reference_data.json"
-    table_name = _get_pointers_table_name()
-    table = DYNAMODB.Table(table_name)
-    scan_kwargs = {}
-    done = False
-    start_key = None
-    existing_nhs_numbers = set()
-
-    # Scan DynamoDB table for all existing NHS numbers
-    while not done:
-        if start_key:
-            scan_kwargs["ExclusiveStartKey"] = start_key
-        response = table.scan(**scan_kwargs)
-        for item in response.get("Items", []):
-            nhs_number = item.get("nhs_number")
-            if nhs_number:
-                existing_nhs_numbers.add(nhs_number)
-        start_key = response.get("LastEvaluatedKey", None)
-        done = start_key is None
-
-    # Calculate how many existing and new NHS numbers to use
-    num_existing = min(
-        int(total_count * proportion_existing), len(existing_nhs_numbers)
-    )
-    num_new = total_count - num_existing
-
-    # Select existing NHS numbers
-    selected_existing_nhs_numbers = list(existing_nhs_numbers)[:num_existing]
-
-    # Generate new NHS numbers that do not overlap with existing
-    new_nhs_numbers = set()
-    # If last_existing_nhs_number is provided, start from the next value
-    if last_existing_nhs_number is not None:
-        try:
-            # Use only the first 9 digits for incrementing
-            start_first9 = int(str(last_existing_nhs_number)[:9])
-        except Exception:
-            start_first9 = 900000000
-
-        class CustomTestNhsNumbersIterator:
-            def __iter__(self):
-                self.first9 = start_first9
-                return self
-
-            def __next__(self):
-                if self.first9 > 999999999:
-                    raise StopIteration
-                checksum = 10
-                while checksum == 10:
-                    self.first9 += 1
-                    nhs_no_digits = list(map(int, str(self.first9)))
-                    checksum = (
-                        sum(
-                            weight * digit
-                            for weight, digit in zip(CHECKSUM_WEIGHTS, nhs_no_digits)
-                        )
-                        * -1
-                        % 11
-                    )
-                nhs_no = str(self.first9) + str(checksum)
-                return nhs_no
-
-        nhs_iter = iter(CustomTestNhsNumbersIterator())
-    else:
-        nhs_iter = iter(TestNhsNumbersIterator())
-    while len(new_nhs_numbers) < num_new:
-        nhs = next(nhs_iter)
-        if nhs not in existing_nhs_numbers and nhs not in new_nhs_numbers:
-            new_nhs_numbers.add(nhs)
-
-    # Prepare output data
-    data = {
-        "new_nhs_numbers": list(new_nhs_numbers),
-        "existing_nhs_numbers": selected_existing_nhs_numbers,
-        "proportion_existing": proportion_existing,
-        "total_count": total_count,
-    }
-    pathlib.Path(out).write_text(json.dumps(data))
-    print(f"Producer data written to {out}")  # noqa: T201
 
 
 def generate_pointer_table_extract(
@@ -236,8 +106,6 @@ if __name__ == "__main__":
 
     fire.Fire(
         {
-            "extract_consumer_data": extract_consumer_data,
-            "generate_producer_data": generate_producer_data,
             "generate_pointer_table_extract": generate_pointer_table_extract,
         }
     )
