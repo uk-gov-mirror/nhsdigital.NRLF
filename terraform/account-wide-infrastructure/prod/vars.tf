040af1a6b0bd9ee11fdd0092a9df23e2aca69543
jackleary <leary.jack@me.com>
2026-01-26 01:04:55 +0000

    NRL-0000 Upgrade ec2 storage to gp3, and up space in prod

--- terraform/account-wide-infrastructure/prod/vars.tf
+++ terraform/account-wide-infrastructure/prod/vars.tf
@@ -68,3 +68,15 @@ variable "use_powerbi_gw_custom_ami" {
   description = "Use custom image for PowerBI GW instance"
   default     = true
 }
+
+variable "root_volume_size" {
+  type        = number
+  description = "Size of the root EBS volume in GB"
+  default     = 256
+}
+
+variable "root_volume_iops" {
+  type        = number
+  description = "IOPS for the root EBS volume"
+  default     = 3000
+}
