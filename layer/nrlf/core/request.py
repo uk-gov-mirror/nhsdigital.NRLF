c982ea82edcbfe930a98a175ee864cdc0e123ec8
Anjali Trace <anjali.trace2@nhs.net>
2026-02-27 16:34:24 +0000

    NRL-1928 Spy on logging and more shiny testing

--- layer/nrlf/core/request.py
+++ layer/nrlf/core/request.py
@@ -19,13 +19,15 @@ def _fetch_ods_app_id_headers(headers: dict[str, str]):
 
     if not ods_code or len(ods_code.strip()) == 0:
         logger.log(
-            LogReference.HANDLER003a, headers_names=case_insensitive_headers.keys()
+            code=LogReference.HANDLER003a,
+            headers_names=list(case_insensitive_headers.keys()),
         )
 
     nrl_app_id = case_insensitive_headers.get("nhsd-nrl-app-id")
     if not nrl_app_id or len(nrl_app_id.strip()) == 0:
         logger.log(
-            LogReference.HANDLER003b, headers_names=case_insensitive_headers.keys()
+            code=LogReference.HANDLER003b,
+            headers_names=list(case_insensitive_headers.keys()),
         )
 
     return ods_code, nrl_app_id
--- layer/nrlf/core/tests/test_authoriser.py
+++ layer/nrlf/core/tests/test_authoriser.py
@@ -1,4 +1,5 @@
 from nrlf.core.authoriser import get_pointer_permissions_v2, parse_permissions_file
+from nrlf.core.logger import LogReference, logger
 from nrlf.core.request import parse_headers
 from nrlf.tests.events import create_headers
 
@@ -22,11 +23,13 @@ def test_authoriser_parse_permission_file_with_permission_file():
 v2_test_lookup_path = "layer/test_permissions/v2"
 
 
-def test_authoriser_get_v2_permissions_with_pointer_types():
+def test_authoriser_get_v2_permissions_with_pointer_types(mocker):
+    spy = mocker.spy(logger, "log")
+
+    expected_lookup_key = "producer/ODS123-app-id/ODS123.json"
     connection_metadata = parse_headers(
         create_headers(ods_code="ODS123", nrl_app_id="ODS123-app-id")
     )
-
     result = get_pointer_permissions_v2(
         connection_metadata=connection_metadata,
         request_path="/producer/DocumentReference/_search",
@@ -35,12 +38,21 @@ def test_authoriser_get_v2_permissions_with_pointer_types():
 
     assert result.get("types") == ["http://snomed.info/sct|736253001"]
 
+    spy.assert_called_with(LogReference.S3PERMISSIONS011, key=expected_lookup_key)
 
-def test_authoriser_parse_v2_permission_file_with_no_permission_file():
+
+def test_authoriser_parse_v2_permission_file_with_no_permission_file(mocker):
+    spy = mocker.spy(logger, "log")
+    expected_lookup_key = "consumer/NotAnApp/NotFound.json"
+
     metadata_result = get_pointer_permissions_v2(
-        connection_metadata=parse_headers(create_headers(ods_code="NotFound")),
+        connection_metadata=parse_headers(
+            create_headers(ods_code="NotFound", nrl_app_id="NotAnApp")
+        ),
         request_path="/consumer/_status",
         lookup_path=v2_test_lookup_path,
     )
 
     assert metadata_result == {}
+
+    spy.assert_any_call(LogReference.S3PERMISSIONS011, key=expected_lookup_key)
