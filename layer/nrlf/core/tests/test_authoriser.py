bcf6bfefbad5387e5b1421a3ec1149343af09ad9
Kate Bobyn <kate.bobyn2@nhs.net>
2026-03-09 00:29:24 +0000

    NRL-1993 use app-level perms if defined

--- layer/nrlf/core/tests/test_authoriser.py
+++ layer/nrlf/core/tests/test_authoriser.py
@@ -27,10 +27,39 @@ def test_authoriser_parse_permission_file_with_permission_file():
     new_callable=mock_open,
     read_data='{"types": ["http://snomed.info/sct|736253001"]}',
 )
-def test_authoriser_get_v2_permissions_with_pointer_types(mock_file, mocker):
+def test_authoriser_get_v2_permissions_with_org_pointer_types(mock_file, mocker):
     spy = mocker.spy(logger, "log")
 
     expected_lookup_key = "producer/ODS123-app-id/ODS123.json"
+    connection_metadata = parse_headers(
+        create_headers(ods_code="ODS123", nrl_app_id="ODS123-app-id")
+    )
+    result = get_pointer_permissions_v2(
+        connection_metadata=connection_metadata,
+        request_path="/producer/DocumentReference/_search",
+    )
+
+    mock_file.assert_called_once_with(
+        f"/opt/python/nrlf_permissions/{expected_lookup_key}"
+    )
+    assert result.get("types") == ["http://snomed.info/sct|736253001"]
+
+    spy.assert_called_with(LogReference.V2PERMISSIONS011, key=expected_lookup_key)
+
+
+@patch(
+    "builtins.open",
+    new_callable=mock_open,
+    read_data='{"types": ["http://snomed.info/sct|736253001"]}',
+)
+@patch("os.path.isfile")
+def test_authoriser_get_v2_permissions_with_app_pointer_types(
+    mock_isfile, mock_file, mocker
+):
+    spy = mocker.spy(logger, "log")
+    mock_isfile.return_value = True
+
+    expected_lookup_key = "producer/ODS123-app-id.json"
     connection_metadata = parse_headers(
         create_headers(ods_code="ODS123", nrl_app_id="ODS123-app-id")
     )
