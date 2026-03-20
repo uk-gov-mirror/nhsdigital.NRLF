ed6254d30512a70a46fc53ca7460c2fcd39f1967
Anjali Trace <anjali.trace2@nhs.net>
2026-03-20 10:56:49 +0000

    NRL-2015 add v1 & v2 headers to all integration tests

--- tests/utilities/api_clients.py
+++ tests/utilities/api_clients.py
@@ -234,26 +234,19 @@ class ProducerTestClient:
         }
 
         if self.config.client_cert:
-            if use_v2:
-                self.request_headers.update(
-                    {
-                        V2Headers.NHSD_END_USER_ORGANISATION_ODS: self.config.connection_metadata.ods_code,
-                        V2Headers.NHSD_NRL_APP_ID: self.config.connection_metadata.nrl_app_id,
-                        NHSD_CORRELATION_ID_HEADER: "test-correlation-id",
-                    }
-                )
-            else:
-                connection_metadata = self.config.connection_metadata.model_dump(
-                    by_alias=True
-                )
-                client_rp_details = connection_metadata.pop("client_rp_details")
-                self.request_headers.update(
-                    {
-                        "NHSD-Connection-Metadata": json.dumps(connection_metadata),
-                        "NHSD-Client-RP-Details": json.dumps(client_rp_details),
-                        "NHSD-Correlation-Id": "test-correlation-id",
-                    }
-                )
+            connection_metadata = self.config.connection_metadata.model_dump(
+                by_alias=True
+            )
+            client_rp_details = connection_metadata.pop("client_rp_details")
+            self.request_headers.update(
+                {
+                    "NHSD-Connection-Metadata": json.dumps(connection_metadata),
+                    "NHSD-Client-RP-Details": json.dumps(client_rp_details),
+                    V2Headers.NHSD_END_USER_ORGANISATION_ODS: self.config.connection_metadata.ods_code,
+                    V2Headers.NHSD_NRL_APP_ID: self.config.connection_metadata.nrl_app_id,
+                    NHSD_CORRELATION_ID_HEADER: "test-correlation-id",
+                }
+            )
 
         self.request_headers.update(self.config.custom_headers)
 
