7d3bae6edc3932b9b8614877a8f65bdc3f430011
Anjali Trace <anjali.trace2@nhs.net>
2026-04-17 11:20:56 +0100

    NRL-2130 remove suspect denies from vault access policy in test to recreate prod issues

--- terraform/backup-infrastructure/modules/aws-backup-destination/variables.tf
+++ terraform/backup-infrastructure/modules/aws-backup-destination/variables.tf
@@ -63,5 +63,5 @@ variable "vault_lock_max_retention_days" {
 variable "changeable_for_days" {
   description = "How long you want the vault lock to be changeable for, only applies to compliance mode. This value is expressed in days no less than 3 and no greater than 36,500; otherwise, an error will return."
   type        = number
-  default     = 14
+  default     = 30
 }
