eae2b38447a229aaf1876372ae2d13a0eedc89ee
Anjali Trace <anjali.trace2@nhs.net>
2026-04-28 10:29:46 +0100

    NRL-2180 remove redundant count line

--- terraform/backup-infrastructure/modules/aws-backup-destination/backup_vault_lock.tf
+++ terraform/backup-infrastructure/modules/aws-backup-destination/backup_vault_lock.tf
@@ -1,5 +1,4 @@
 resource "aws_backup_vault_lock_configuration" "vault_lock" {
-  count               = 1
   backup_vault_name   = aws_backup_vault.vault.name
   changeable_for_days = var.vault_lock_type == "compliance" ? var.changeable_for_days : null # providing changeable_for_days === enabling compliance mode
   max_retention_days  = var.vault_lock_max_retention_days
