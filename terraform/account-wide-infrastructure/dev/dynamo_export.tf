6cdfa8b7d70b0e8e3e6af574a56f7d1f06e98681
Anjali Trace <anjali.trace2@nhs.net>
2026-04-30 16:33:42 +0100

    NRL-2002 Remove unused variables

--- terraform/account-wide-infrastructure/dev/dynamo_export.tf
+++ terraform/account-wide-infrastructure/dev/dynamo_export.tf
@@ -1,5 +1,5 @@
 module "dynamo_export" {
-  source             = "./modules/dynamo_export"
+  source             = "../modules/dynamo_export"
   name_prefix        = "nhsd-nrlf--dev"
   environment        = "dev"
   pointer_table_name = module.dev-pointers-table.table_name
--- terraform/account-wide-infrastructure/modules/dynamo_export/variables.tf
+++ terraform/account-wide-infrastructure/modules/dynamo_export/variables.tf
@@ -12,13 +12,3 @@ variable "pointer_table_name" {
   type        = string
   description = "Name of the pointer table to export"
 }
-
-variable "asset_bucket" {
-  type        = string
-  description = "Name of the bucket that holds lambda zips"
-}
-
-variable "asset_version" {
-  type        = string
-  description = "Version for the lambda zips to use during deployment"
-}
